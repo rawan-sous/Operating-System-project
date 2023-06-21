1e88374d99f11970dda96e8936b49cd3c45e2866
Rivan jaradat <1200081@student.birzeit.edu>
2023-06-21 20:45:05 +0300

    Update thread.py

--- thread.py
+++ thread.py
@@ -48,6 +48,30 @@ class LinkedList:
             print(f"Thread ID: {current.id}, Name: {current.name}, Current State: {current.currentState}")
             current = current.next
 
+    def creatThread(self ,threadname,id,enteryPoint):
+        thread=Thread(threadname,id,enteryPoint)
+        
+        self.append(thread)
+    def terminate_thread(self,Id):
+       current = self.head
+       previous = None
+
+       while current:
+            if current.id == Id:   
+                break
+            previous = current
+            current = current.next
+    def thread_command(self):
+        i = 0
+        while True:
+            thread_id = input(f'Enter thread ID for thread {i+1}: ')
+            name = input(f'Enter thread name for thread {i+1}: ')
+            entry_point = input(f'Enter entry point for thread {i+1}: ')
+            self.create_thread(name, thread_id, entry_point)
+            i += 1
+
+        
+
 class Thread(threading.Thread):
  
     link = LinkedList()
