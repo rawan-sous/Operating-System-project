a140ee38d9506bb14f076558acfb85cad7f90330
Rivan jaradat <1200081@student.birzeit.edu>
2023-06-25 20:38:40 +0300

    Update thread.py

--- thread.py
+++ thread.py
@@ -1,4 +1,5 @@
 import threading
+
 # Node class representing a node in the linked list
 class Node:
     def __init__(self, id, name, currentState):
@@ -42,25 +43,23 @@ class LinkedList:
             prev = current
             current = current.next
 
-    def print_list(self):
+    def create_thread(self, thread_name, thread_id, entry_point):
+        thread = Thread(thread_name, thread_id, entry_point)
+        self.append(thread_id, thread_name, "Created")
+        thread.start()
+
+    def terminate_thread(self, thread_id):
         current = self.head
+        prev = None
         while current:
-            print(f"Thread ID: {current.id}, Name: {current.name}, Current State: {current.currentState}")
+            if current.id == thread_id:
+                self.update_thread_state(thread_id, "Terminated")
+                current.join()  # Wait for the thread to complete before termination
+                self.delete(thread_id)
+                return
+            prev = current
             current = current.next
-
-    def creatThread(self ,threadname,id,enteryPoint):
-        thread=Thread(threadname,id,enteryPoint)
-        
-        self.append(thread)
-    def terminate_thread(self,Id):
-       current = self.head
-       previous = None
-
-       while current:
-            if current.id == Id:   
-                break
-            previous = current
-            current = current.next
+    
     def thread_command(self):
         i = 0
         while True:
@@ -70,16 +69,51 @@ class LinkedList:
             self.create_thread(name, thread_id, entry_point)
             i += 1
 
-        
+    def update_thread_state(self, thread_id, state):
+        current = self.head
+        while current:
+            if current.id == thread_id:
+                current.currentState = state
+                return
+            current = current.next
+
+    def print_thread_list(self):
+        current = self.head
+        while current:
+            print(f"Thread ID: {current.id}, Name: {current.name}, Current State: {current.currentState}")
+            current = current.next
+
+ 
+    def handle_error(self, error_message):
+        print(f"Error: {error_message}")
+
+    def handle_thread_creation(self, thread_name, thread_id, entry_point):
+        print(f"Thread {thread_name} with ID {thread_id} is created with entry point {entry_point}")
+
+    def execute_threads(self):
+        current = self.head
+        while current:
+            self.update_thread_state(current.id, "Running")
+            current.start()
+            current = current.next
+
 
 class Thread(threading.Thread):
- 
-    link = LinkedList()
-
-    def __init__(self, name, ID,entry_point):
+    def __init__(self, name, ID, entry_point):
         threading.Thread.__init__(self)
         self.thread_name = name
         self.thread_ID = ID
         self.entry_point = entry_point
+
     def run(self):
-        self.entry_point()
+        try:
+            self.entry_point()
+        except Exception as e:
+            print(f"Error in thread {self.thread_name}: {str(e)}")
+        finally:
+            self.link.update_thread_state(self.thread_ID, "Terminated")
+
+    def entry_point(self):
+        print(f"Thread {self.thread_name} is running")
+
+
